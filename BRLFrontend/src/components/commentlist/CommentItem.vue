41b3c121e21662d6c2b3f3e38809a17d3b3b46e3
Ted Bjurlin <tbjurlin@rocketsoftware.com>
2025-12-04 16:53:34 -0600

    Scaffolded commentlist

---
+++ BRLFrontend/src/components/commentlist/CommentItem.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="comment-container">
+
+    </div>
+</template>
+
+<style scoped>
+@import "./styles/CommentItem.css";
+</style>
---
+++ BRLFrontend/src/components/commentlist/CommentList.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="comment-list">
+
+    </div>
+</template>
+
+<style scoped>
+@import "./styles/CommentList.css";
+</style>
