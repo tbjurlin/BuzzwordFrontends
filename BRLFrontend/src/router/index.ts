2a9489f1561bd392d96c632f884e8ce79c816540
Ted Bjurlin <tbjurlin@rocketsoftware.com>
2025-12-10 12:54:51 -0500

    fixed index

--- BRLFrontend/src/router/index.ts
+++ BRLFrontend/src/router/index.ts
@@ -70,39 +70,4 @@ router.beforeEach((to, from, next) => {
   next()
 })
 
-// Navigation guard to check authentication
-router.beforeEach((to, from, next) => {
-  const { checkAuth, setToken } = useAuth()
-  
-  // Check if there's a token in the URL (coming back from FlowerSSO)
-  const urlParams = new URLSearchParams(window.location.search)
-  const tokenFromUrl = urlParams.get('token')
-  
-  if (tokenFromUrl) {
-    // Store the token from FlowerSSO
-    setToken(tokenFromUrl)
-    // Clean up URL to remove token parameter
-    const cleanUrl = window.location.origin + to.fullPath.split('?')[0]
-    window.history.replaceState({}, '', cleanUrl)
-    next()
-    return
-  }
-  
-  // Check if user is authenticated
-  const isAuthenticated = checkAuth()
-  
-  // If not authenticated, redirect to FlowerSSO with return URL
-  if (!isAuthenticated) {
-    // Build the redirect URL to come back to BRL after login
-    const currentUrl = window.location.origin + to.fullPath
-    const ssoUrl = `${FlowerSSOUrl}?redirect=${encodeURIComponent(currentUrl)}`
-    
-    // Prevent navigation and redirect to SSO
-    window.location.href = ssoUrl
-    return // Don't call next(), just redirect
-  }
-  
-  next()
-})
-
 export default router
