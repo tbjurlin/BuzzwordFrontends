a3ae0ed834d10acca92e4ba6b507a9a93f3c1178
Janniebeth <JMelendeztorres@rocketsoftware.com>
2025-12-11 13:37:31 -0500

    slowly integrating config file information for the about us page

--- BRLFrontend/src/views/AboutUsView.vue
+++ BRLFrontend/src/views/AboutUsView.vue
@@ -1,16 +1,37 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted } from 'vue'
 import { ref, computed } from 'vue'
 import { useRouter } from 'vue-router';
 import VueFeather from 'vue-feather';
+import { useAboutUs } from '@/composables/useAboutUs';
+import type { AboutUs } from '@/types';
+
+const aboutUs = ref<AboutUs>();
+
+onMounted(() => {
+    const { getAboutUs } = useAboutUs();
+
+    getAboutUs(
+        (data: AboutUs) => {
+            aboutUs.value = data;
+        },
+        (reason: any) => {
+            console.error("Failed to fetch About Us information:", reason);
+        }
+    );
+});
 
 </script>
+
+
 
 <template>
     <!--Config file information from the BRL API-->
     <div class="about-us-view">
         <h1>About Us</h1>
-        <p>...</p>
+        <div class="about-us-content">
+            
+         </div>
     </div>
 </template>
 
